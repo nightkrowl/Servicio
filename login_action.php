ce984751ca4c14a9266161d593b28d990fda8550
JonathanAlvarado <jonathan.aam92@gmail.com>
2013-07-26 11:46:46 -0500

    login en web service

---
+++ login_action.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	login();
+}
+
+function login(){
+	$data = array('usuario' => $_POST['usuario'], 'contrasena'=>$_POST['contrasena']);
+
+	//url a web service
+	$url = "http://localhost/ws_login.php";
+	//curl_handle
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$return = curl_exec($ch);
+	curl_close($ch);
+	$result = json_decode($return, true);
+	
+	if ($result['sesion'] == True){
+		header('Location: /admin.php');
+	}	
+}
+?>
